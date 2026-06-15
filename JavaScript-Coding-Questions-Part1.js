fce29eed5caf92a1134af28f87f37e7f2a4d1e18
SAMAR <96787965+samarsekhar@users.noreply.github.com>
2026-06-15 20:30:09 +0530

    Update JavaScript-Coding-Questions-Part1.js

--- JavaScript-Coding-Questions-Part1.js
+++ JavaScript-Coding-Questions-Part1.js
@@ -27,9 +27,9 @@ function findLargest(arr) {
 
 // 4. FizzBuzz
 // Problem: Print numbers from 1 to 100.
-Multiple of 3 → "Fizz"
-Multiple of 5 → "Buzz"
-Multiple of both → "FizzBuzz"
+// Multiple of 3 → "Fizz"
+// Multiple of 5 → "Buzz"
+// Multiple of both → "FizzBuzz"
 for (let i = 1; i <= 100; i++) {
   if (i % 15 === 0) console.log("FizzBuzz");
   else if (i % 3 === 0) console.log("Fizz");
